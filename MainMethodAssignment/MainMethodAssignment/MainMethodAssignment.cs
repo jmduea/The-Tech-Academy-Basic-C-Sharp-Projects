f641402fa7d17f016fe4ab19dcf533f862599fee
jmduea <119540897+jmduea@users.noreply.github.com>
2023-03-01 12:56:21 -0600

    Code for the Main Method submission assignment

---
+++ MainMethodAssignment/MainMethodAssignment/MainMethodAssignment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MainMethodAssignment
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the MathOperations class
+            MathOperations math = new MathOperations();
+
+            // call the first method with an integer input
+            int result1 = math.DoMath(10);
+            Console.WriteLine($"Result 1: {result1}");
+
+            // call the second method with a decimal input
+            int result2 = math.DoMath(4.5M);
+            Console.WriteLine($"Result 2: {result2}");
+
+            // call the third method with a string input
+            int result3 = math.DoMath("20");
+            Console.WriteLine($"Result 3: {result3}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/MathOperations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace MainMethodAssignment
+{
+    public class MathOperations
+    {
+        // method that takes in an integer, creates a math operation for this integer, then returns the answer as an integer
+        public int DoMath(int num)
+        {
+            // create a math operation for the input integer (in this case, add 5 to it)
+            int result = num + 5;
+            // return the result
+            return result;
+        }
+
+        // method that takes in a decimal, creates a different math operation for it, then returns the answer as an integer
+        public int DoMath(decimal num)
+        {
+            // create a math operation for input decimal (in this case, multiply by 2)
+            decimal result = num * 2;
+            // cast the decimal result to an integer and return it
+            return (int)result;
+        }
+
+        // method that takes in a string, converts it to an integer if possible, does a different math operation on it, then returns the answer as an integer
+        public int DoMath(string numStr)
+        {
+            int num;
+            // try to convert the string input to an integer
+            if (int.TryParse(numStr, out num))
+            {
+                // create a math operation for the input integer (in this case, subtract 3)
+                int result = num - 3;
+                // return the result
+                return result;
+            }    
+            else
+            {
+                // if the input string cannot be converted to an integer, return -1
+                return -1;
+            }
+        }
+    }
+}
