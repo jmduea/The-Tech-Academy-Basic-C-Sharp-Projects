9686dad4130fead32c54c3d37aeb2389ad983722
jmduea <119540897+jmduea@users.noreply.github.com>
2023-03-01 12:28:46 -0600

    Code for Calling Methods Submission Assignment

---
+++ CallingMethods/CallingMethods/CallingMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace CallingMethods
+{
+    class CallingMethods
+    {
+        static void Main(string[] args)
+        {
+            // ask the user what number they want to do the math operation on
+            Console.Write("Enter a number to perform math operations on: ");
+            int num = int.Parse(Console.ReadLine());
+
+            // create an instance of the MathOperations class
+            MathOperations math = new MathOperations();
+
+            // call each method in turn, passing the user input to the method
+            int result1 = math.MultiplyByTwo(num);
+            int result2 = math.AddTen(num);
+            int result3 = math.SubtractFive(num);
+
+            // display the returned integers to the screen
+            Console.WriteLine($"Result 1: {result1}");
+            Console.WriteLine($"Result 2: {result2}");
+            Console.WriteLine($"Result 3: {result3}");
+            
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace CallingMethods
+{
+    public class MathOperations
+    {
+        // multiply the num variable by 2
+        public int MultiplyByTwo(int num)
+        {
+            return num * 2;
+        }
+
+        // add ten to the num variable
+        public int AddTen(int num)
+        {
+            return num + 10;
+        }
+
+        // subtract five from the numb variable
+        public int SubtractFive(int num)
+        {
+            return num - 5;
+        }
+    }
+}
