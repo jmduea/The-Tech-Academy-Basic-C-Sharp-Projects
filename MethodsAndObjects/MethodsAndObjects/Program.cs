c4cd0accfac28de5511fddfd0b2addf185f807f2
jmduea <119540897+jmduea@users.noreply.github.com>
2023-03-01 23:39:14 -0600

    Code for Methods and Objects Assignment

---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace MethodsAndObjects
+{
+    // Define a class called Person with two properties - FirstName and LastName
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Define a method called SayName that writes the full name to the console
+        public void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+
+    // Define a class called Employee which inherits from the Person class
+    class Employee : Person
+    {
+        // Add a new property to Employee called Id
+        public int Id { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with a first name of "Sample" and a last name of "Student"
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                Id = 123456
+            };
+
+            // Call the SayName method on the Employee object, which will output the first and last name
+            employee.SayName();
+
+            // Keep the console window open
+            Console.ReadLine();
+        }
+    }
+}
