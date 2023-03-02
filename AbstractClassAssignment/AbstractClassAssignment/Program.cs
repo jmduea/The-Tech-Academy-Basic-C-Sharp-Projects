e5db09f3f15e45aed2cb442eaf4f5dce01476f35
jmduea <119540897+jmduea@users.noreply.github.com>
2023-03-02 02:01:15 -0600

    Code for Abstract Class Assignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace AbstractClassAssignment
+{
+    abstract class Person
+    {
+        // Add two properties to Person - firstName and lastName
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // Add a method called SayName
+        public void SayName()
+        {
+            Console.WriteLine($"Name: {firstName} {lastName}");
+        }
+    }
+
+    // Define a class called Employee which inherits from the Person class
+    class Employee : Person
+    {
+        // Implement the SayName method in the Employee class
+        public new void SayName()
+        {
+            Console.WriteLine($"Employee Name: {firstName} {lastName}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with a first name of "Sample and a last name of "Employee"
+            Employee employee = new Employee
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+
+            // Call the overridden SayName method on the Employee object, which will output the employee's name to the console
+            employee.SayName();
+
+            // Keep the console window open
+            Console.ReadLine();
+        }
+    }
+}
