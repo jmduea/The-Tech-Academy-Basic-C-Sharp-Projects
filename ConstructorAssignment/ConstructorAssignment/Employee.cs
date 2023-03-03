47291e8e15b1bab5361e7a8f274909f429505372
jmduea <119540897+jmduea@users.noreply.github.com>
2023-03-03 01:12:00 -0600

    code for Constructor assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+
+        // Define a constructor that takes three parameters
+        public Employee(string firstName, string lastName, string position)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Position = position;
+        }
+
+        // Define a constructor that takes two parameters and chains to the previous constructor
+        public Employee(string firstName, string lastName) : this(firstName, lastName, "Employee")
+        {
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a const variable
+            const string companyName = "ABC Corp";
+
+            // Create a variable using the keyword "var"
+            var employee = new Employee("John", "Doe", "Manager");
+
+            // Output the employee's information to the console
+            Console.WriteLine($"Name: {employee.FirstName} {employee.LastName}, Position: {employee.Position}");
+
+            // Wait for the user to press a key before closing the console window
+            Console.ReadKey();
+        }
+    }
+}
